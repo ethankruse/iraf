7daacee25bde2e0336a0af72202f1310032e7470
Ethan Kruse <eakruse@uw.edu>
2016-03-29 16:29:54 -0700

    Ok, definitely have a working interface now. Am working on implementing all the keyboard shortcut functions!

--- implot.py
+++ implot.py
@@ -3,24 +3,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 from astropy.io import fits
 from matplotlib.widgets import Button, CheckButtons, Slider
-import threading
 
 
-im_set = {'fig': None, 'ax': None, 'lineplot': True,
+im_set = {'fig': None, 'ax': None, 'sax': None, 'lineplot': True,
           'line': None, 'im': None, 'ndim': None, 'navg': None, 'ncols': None,
-          'nlines': None, 'image': None, 'index': None, 'cid': None}
+          'nlines': None, 'image': None, 'index': None, 'cid': None,
+          'input': '', 'iax': None, 'overplot': False, 'sid': None,
+          '_slider': None, 'stat': None, 'xdata': None, 'ydata': None}
 
-
-def implot_redraw():
-    im_set['ax'].cla()
-
-
-# XXX: look into multi-threading with raw_input() to handle things!
 
 def implot_plot():
+    if not im_set['overplot']:
+        im_set['ax'].cla()
+
     if im_set['lineplot']:
         y1 = max(0, min(im_set['nlines'] - 1, im_set['line']))
-        y2 = max(1, min(im_set['nlines'], im_set['line'] + im_set['ndim']))
+        y2 = max(1, min(im_set['nlines'], im_set['line'] + im_set['navg']))
         if im_set['ndim'] == 1:
             yplot = im_set['im']
         else:
@@ -29,7 +27,7 @@ def implot_plot():
         xplot = np.arange(im_set['ncols'])
     else:
         x1 = max(0, min(im_set['ncols'] - 1, im_set['line']))
-        x2 = max(1, min(im_set['ncols'], im_set['line'] + im_set['ndim']))
+        x2 = max(1, min(im_set['ncols'], im_set['line'] + im_set['navg']))
         if im_set['ndim'] == 1:
             yplot = im_set['im'][x1:x2]
         else:
@@ -41,9 +39,11 @@ def implot_plot():
     #    call strcpy(format, fmt, SZ_FNAME)
 
     im_set['ax'].plot(xplot, yplot)
+    im_set['xdata'] = xplot
+    im_set['ydata'] = yplot
 
     if im_set['ndim'] > 1:
-        if im_set['ndim'] > 1:
+        if im_set['navg'] > 1:
             if im_set['lineplot']:
                 txt = "lines"
                 i1 = y1
@@ -55,7 +55,7 @@ def implot_plot():
                 i2 = x2
                 sz = im_set['ncols']
             title = "Average of {0} {1:d} to {2:d} of {3} in\n{4}"
-            title = title.format(txt, i1, i2, sz,
+            title = title.format(txt, i1, i2-1, sz,
                                  im_set['image'][im_set['index']])
         else:
             if im_set['lineplot']:
@@ -75,57 +75,166 @@ def implot_plot():
     im_set['fig'].canvas.draw_idle()
 
 
-def get_input(prompt, event):
-    import time
-    time.sleep(0.1)
-    raw_input(prompt)
-    event.set()
-
-
-
 def implot_keypress(event):
-    print 'press', event.key
+    # pressed for the second time
+    if im_set['stat'] is not None:
+        event.key = 's'
+
+    # user input is complete and was one of the extended options
+    if event.key == 'enter' and len(im_set['input']):
+        full_inp = im_set['input']
+        print 'complete'
+        print full_inp
+        im_set['input'] = ''
+        im_set['iax'].set_text('')
+        im_set['fig'].canvas.draw_idle()
+        # XXX: handle longer inputs
+        return
+
+    # user is continuing their long input
+    if len(im_set['input']):
+        # ignore keypress events like 'up', 'down', 'shift'.
+        if len(event.key) == 1:
+            im_set['input'] += event.key
+
+        im_set['iax'].set_text(im_set['input'])
+        im_set['fig'].canvas.draw_idle()
+        return
+    # user is beginning a long input
+    if event.key == ':':
+        im_set['input'] += ':'
+        im_set['iax'].set_text(im_set['input'])
+        # XXX: make a global variable of 'previous inputs' that resets when
+        # you hit the enter key. Then interpret that.
+        # Also make use of the stdout manipulation to be printing what you're
+        # typing on the command line.
+
+    # wants to plot lines
+    if event.key == 'l':
+        # but we're currently plotting columns
+        if not im_set['lineplot']:
+            was_set = im_set['overplot']
+            # turn overplot off
+            if im_set['overplot']:
+                im_set['_check'].set_active(0)
+                im_set['overplot'] = False
+            # which column to plot
+            fracthru = im_set['line'] * 1. / im_set['ncols']
+            im_set['line'] = int(im_set['nlines'] * fracthru)
+            im_set['lineplot'] = True
+            # redo the slider
+            implot_remove_slider()
+            implot_make_slider()
+
+            implot_plot()
+            if was_set:
+                im_set['_check'].set_active(0)
+                im_set['overplot'] = True
+
+    # wants to plot columns
+    if event.key == 'c':
+        # but we're currently plotting lines
+        if im_set['lineplot']:
+            if im_set['nlines'] == 1:
+                print '\007'
+                return
+
+            # turn overplot off
+            was_set = im_set['overplot']
+            if im_set['overplot']:
+                im_set['_check'].set_active(0)
+                im_set['overplot'] = False
+            # which line to plot
+            fracthru = im_set['line']*1. / im_set['nlines']
+            im_set['line'] = int(im_set['ncols'] * fracthru)
+            im_set['lineplot'] = False
+            # redo the slider
+            implot_remove_slider()
+            implot_make_slider()
+
+            implot_plot()
+            if was_set:
+                im_set['_check'].set_active(0)
+                im_set['overplot'] = True
+
+    # go to previous image in list
+    if event.key == 'm':
+        if im_set['index'] > 0:
+            im_set['index'] -= 1
+            implot_open_image()
+
+    # go to next image in list
+    if event.key == 'n':
+        if im_set['index'] < len(im_set['image']) - 1:
+            im_set['index'] += 1
+            implot_open_image()
+
+    if event.key == 'p':
+        # XXX: implement impprofile.x
+        """
+        case 'p':
+        # Profile analysis.
+        x1 = x
+        y1 = y
+        call printf (again)
+        if (clgcur ("gcur", x2, y2, wcs, key, command,
+            SZ_FNAME) == EOF)
+            next
+
+        call imp_profile (gp, Memr[xnew], Memr[ynew], npix,
+            x1, y1, x2, y2, sl, sline)
+        call printf (Memc[sl_getstr(sl,sline)])
+        """
+        pass
+
+    # print statistics on a region
+    if event.key == 's':
+        if (event.inaxes is not im_set['ax'] or event.xdata is None or
+                    event.ydata is None):
+            im_set['stat'] = None
+            return
+
+        if im_set['stat'] is None:
+            im_set['stat'] = (event.xdata, event.ydata)
+            print 'Again'
+            return
+
+        # have 2 successive presses, calculate the statistics
+        x1 = min(im_set['stat'][0], event.xdata)
+        x2 = max(im_set['stat'][0], event.xdata)
+
+        region = np.where((im_set['xdata'] >= x1) & (im_set['xdata'] <= x2))[0]
+        if len(region) == 0:
+            ind1 = np.abs(im_set['xdata'] - x1).argmin()
+            if ind1 != len(im_set['xdata']):
+                region = np.array([ind1, ind1+1])
+            else:
+                region = np.array([ind1, ind1 - 1])
+        mean = im_set['ydata'][region].mean()
+        std = im_set['ydata'][region].std()
+        sum = im_set['ydata'][region].sum()
+        median = np.median(im_set['ydata'][region])
+        outstr = "Median={0:g}, mean={1:g}, rms={2:g}, sum={3:g}, npix={4:d}"
+        print outstr.format(median, mean, std, sum, len(region))
+
+
+    # test for other letter shortcuts
     if event.key == 'x':
         im_set['ax'].plot(np.random.randn(50) * 500, np.random.randn(50) * 200)
     if event.key == 'q':
         print 'q'
         im_set['ax'].set_xlim(np.random.randn() * 50,
                               np.random.randn() * 50 + 1500)
-    #plt.draw()
+
     im_set['fig'].canvas.draw_idle()
 
-    if event.key == ':':
-        #im_set['fig'].canvas.mpl_disconnect(im_set['cid'])
-        #inp = raw_input('What do you want?')
-        #print inp
-        #im_set['cid'] = im_set['fig'].canvas.mpl_connect('key_press_event',
-        #                                       implot_keypress)
 
-        #im_set['fig'].canvas.stop_event_loop()
-        #inp = raw_input("What do you want?")
-        #print inp
-        #im_set['fig'].canvas.start_event_loop()
-        #ready = threading.Event()
-        #input_thread = threading.Thread(target=get_input, args=("What do you want?", ready))
-        #print 'waiting'
-        #input_thread.start()
-        #ready.wait()
-        pass
-        # XXX: make a global variable of 'previous inputs' that resets when
-        # you hit the enter key. Then interpret that.
-        # Also make use of the stdout manipulation to be printing what you're
-        # typing on the command line.
-
-
-    print 'made it through'
-
-
-def test(event):
-    im_set['ax'].set_xlim(np.random.randn()*50, np.random.randn()*50 + 1500)
-    im_set['ax'].plot(np.random.randn(50) * 500, np.random.randn(50) * 200)
-    #plt.draw()
-    im_set['fig'].canvas.draw_idle()
-    print 'hi'
+def test(label):
+    if label == 'Overplot':
+        if not im_set['overplot']:
+            im_set['overplot'] = True
+        else:
+            im_set['overplot'] = False
 
 
 def implot_change_line(value):
@@ -134,7 +243,82 @@ def implot_change_line(value):
     pass
 
 
+def implot_open_image():
+    # open the image
+    try:
+        hdulist = fits.open(im_set['image'][im_set['index']])
+    except IOError:
+        print "Error reading image {0} ...".format(
+            im_set['image'][im_set['index']])
+        # XXX: go to next image
+
+    im_set['im'] = hdulist[0].data
+    hdulist.close()
+
+    if im_set['im'] is None:
+        # XXX: call error (1, "image has no pixels")
+        pass
+
+    im_set['ncols'] = im_set['im'].shape[0]
+    if len(im_set['im'].shape) > 1:
+        im_set['nlines'] = im_set['im'].shape[1]
+        im_set['ndim'] = 2
+    else:
+        im_set['nlines'] = 1
+        im_set['ndim'] = 1
+
+    if im_set['line'] is None:
+        im_set['line'] = max(0, min(im_set['nlines'],
+                                    (im_set['nlines'] + 1) / 2) - 1)
+        im_set['lineinit'] = im_set['line']
+        im_set['colinit'] = max(0,
+                                min(im_set['ncols'], (im_set['ncols'] + 1) / 2) - 1)
+    # redo the slider
+    implot_remove_slider()
+    implot_make_slider()
+
+    implot_plot()
+
+
+def implot_remove_slider():
+    if im_set['_slider'] is None:
+        return
+
+    im_set['_slider'].disconnect(im_set['sid'])
+    im_set['fig'].delaxes(im_set['sax'])
+    im_set['sax'] = None
+    im_set['sid'] = None
+    im_set['_slider'] = None
+
+
+def implot_make_slider():
+    if im_set['lineplot']:
+        txt = 'Line'
+    else:
+        txt = 'Column'
+    # slider can't adjust bounds, so make sure it has room for the largest
+    # dimension, then just prevent the smaller dimension from having higher
+    # values
+    if im_set['lineplot']:
+        smax = im_set['nlines'] - 1
+    else:
+        smax = im_set['ncols'] - 1
+    # smax = max(im_set['ncols'], im_set['nlines']) - 1
+    im_set['sax'] = plt.axes([0.83, 0.1, 0.15, 0.05])
+    slider = Slider(im_set['sax'], txt, 0, smax, valinit=im_set['line'], valfmt='%.0f')
+    im_set['sid'] = slider.on_changed(implot_change_line)
+    slider.vline.set_visible(False)
+
+    im_set['_slider'] = slider
+
+
 def implot(*args, **kwargs):
+    # XXX: where does this go?
+    # Disable default Matplotlib shortcut keys:
+    keymaps = [param for param in plt.rcParams if param.find('keymap') >= 0]
+    for key in keymaps:
+        plt.rcParams[key] = ''
+
     params = loadparams(*args, **kwargs)
     im_set['image'] = file_handler(params['image'].value)
     im_set['line'] = params['line'].value
@@ -144,7 +328,6 @@ def implot(*args, **kwargs):
     device = params['device'].value
 
     logscale = False
-    overplot = False
     erase = False
     xnticks = 5
     ynticks = 5
@@ -153,7 +336,7 @@ def implot(*args, **kwargs):
     linetype = 1
     color = 1
     p_navg = 1
-    im_set['ndim'] = 1
+    im_set['navg'] = 1
 
     # we couldn't find any images to plot
     if len(im_set['image']) == 0:
@@ -169,70 +352,26 @@ def implot(*args, **kwargs):
     # which image we're examining now
     im_set['index'] = 0
 
-    # open the image
-    try:
-        hdulist = fits.open(im_set['image'][im_set['index']])
-    except IOError:
-        print "Error reading image {0} ...".format(
-            im_set['image'][im_set['index']])
-        # XXX: go to next image
-
-    im_set['im'] = hdulist[0].data
-    if im_set['im'] is None:
-        # XXX: call error (1, "image has no pixels")
-        pass
-
-    im_set['ncols'] = im_set['im'].shape[0]
-    if len(im_set['im'].shape) > 1:
-        im_set['nlines'] = im_set['im'].shape[1]
-        im_set['ndim'] = 2
-    else:
-        im_set['nlines'] = 1
-        im_set['ndim'] = 1
-
-    if im_set['line'] is None:
-        im_set['line'] = max(1,
-                             min(im_set['nlines'], (im_set['nlines'] + 1) / 2))
-
-    if step is None or step < 1:
-        step = max(1, im_set['nlines'] / 10)
-
-    if not overplot:
-        implot_redraw()
-
-    implot_plot()
+    # open the image and plot it
+    implot_open_image()
 
     im_set['cid'] = fig.canvas.mpl_connect('key_press_event', implot_keypress)
 
-    fig.subplots_adjust(top=0.8)
+    fig.subplots_adjust(right=0.8)
 
-    #rax = plt.axes([0.85, 0.4, 0.15, 0.15])
-    #bnext = Button(rax, 'Next')
-    #nax = plt.axes([0.85, 0.6, 0.15, 0.15])
-    #check = CheckButtons(nax, ('2 Hz', '4 Hz', '6 Hz'), (False, True, True))
-    #check.on_clicked(test)
-    #bnext.on_clicked(test)
+    inptxt = fig.text(0.02, 0.02, '')
+    im_set['iax'] = inptxt
 
-    #im_set['._check'] = check
-    # im_set['._button'] = bnext
+    nax = plt.axes([0.85, 0.6, 0.15, 0.15])
+    check = CheckButtons(nax, ('Overplot',), (im_set['overplot'], ))
+    check.on_clicked(test)
 
-    if im_set['lineplot']:
-        txt = 'line'
-        smax = im_set['nlines']
-    else:
-        txt = 'column'
-        smax = im_set['ncols']
-    sax = plt.axes([0.2, 0.85, 0.15, 0.1])
-    slider = Slider(sax, 'Central {0}'.format(txt), 0, smax-1, valinit=im_set['line'], valfmt='%.0f')
-    slider.on_changed(implot_change_line)
-
-    im_set['_slider'] = slider
+    im_set['_check'] = check
 
 
-    overplot = False
 
     plt.show(block=False)
 
-    hdulist.close()
+
 
     return params
